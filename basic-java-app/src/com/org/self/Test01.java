d56ec25aa041ae8d153ea43a75e30dc0ffd8d733
nimanshu <nimanshu.parekh@gmail.com>
2021-07-29 16:02:34 +0400

    test

---
+++ basic-java-app/src/com/org/self/Test01.java
@@ -0,0 +1,5 @@
+package com.org.self;
+
+public class Test01 {
+
+}
--- basic-java-app/src/com/org/self/TestClass.java
+++ basic-java-app/src/com/org/self/TestClass.java
@@ -1,6 +1,11 @@
-
+package com.org.self;
 public class TestClass {
 public static void main (String [] args) {
 	System.out.println("Hello Nimanshu");
+	
+}
+
+public static void test() {
+	System.out.println("test nimanshu");
 }
 }
